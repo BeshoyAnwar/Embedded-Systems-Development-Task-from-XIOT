c5ec429cfa2b18e49c7d06c2d7e6c92aa641f7d3
BeshoyAnwar <32778960+BeshoyAnwar@users.noreply.github.com>
2018-06-27 19:22:23 +0200

    final main layer of the app
    **include the initiating functions of the modules
    **the super loop is empty as every thing done with 2 ISR(interrupt from INT0{connected to switch},interrupt from Timer0)

--- source_code/main.c
+++ source_code/main.c
@@ -1,7 +1,18 @@
+#include "ADC.h"
+#include "Timer0.h"
+#include "inputOutput.h"
+#include "UART.h"
+
+
 int main(void)
 {
-  // initiating functions
-	while (1){
-  
-  }
+    INT0_init();                     // Enable external INT0
+    output_pin_init();	            // init PC0 as output pin
+    timer0_init_CTC_mode(250);     // start the timer
+    UART_init();                  // initialize UART driver
+    ADC_init(); 	         // initialize ADC driver
+    while(1)
+    {
+
+    }
 }
